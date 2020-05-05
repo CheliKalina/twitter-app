0158c353846ec2206ab91f6b992dafa23e6f88c2
Chel iKalina <43254524+CheliKalina@users.noreply.github.com>
2020-05-05 13:48:33 +0300

    Mile 1 - initial commit

---
+++ src/components/NewTweet.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import '../App.css';
+
+
+// class NewTweet extends React.Component {
+
+// }
+
+function tweet() {
+    return (
+        <div className="tweet">
+            <p> What do you have to say...</p>
+            <Button>Tweet</Button>
+
+        </div>
+    )
+  }
+  
+  export default tweet;
+  

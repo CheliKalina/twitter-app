d884bbedfae6323e386c1b44e7eac0661b28c3f9
Chel iKalina <43254524+CheliKalina@users.noreply.github.com>
2020-05-07 13:29:02 +0300

    Mile 1 - second commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
 import React from 'react';
 import './App.css';
-import NewTweet from './components/NewTweet.jsx'
-import Menu from './components/Nav.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Menu from './components/Nav.jsx'
+import CreateTweet from './components/CreateTweet.jsx';
+import TweetList from './components/TweetList.jsx';
 
-
-function App() {
-  return (
-    <div className="App">
-    <Container>
-  <Menu/>
-  <Row >
-      <Col>
-  <NewTweet/>
-    </Col>
-  </Row>
-</Container>
-      
-    </div>
-
-    
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shouldPresentTweet: true
+    }
+  }
+  render() {
+    const { shouldPresentTweet } = this.state;
+    return (
+      <div className="App">
+        <Container>
+          <Menu />
+          <Row >
+            <CreateTweet
+              onFormSubmit={(values) => { this.setState({ shouldPresentTweet: values.tweet <= 1 }) }}
+            ></CreateTweet>
+            <TweetList />
+          </Row>
+        </Container> 
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+// // import charErr from './components/charErr.jsx'
+// import CreateTweet from './components/CreateTweet.jsx'
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Menu from './components/Nav.jsx'
+// import TweetList from './components/TweetList'
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <CreateTweet/>
+//       {/* <Container>
+//         <Menu />
+//         <Row >
+//             <CreateTweet />
+//             <TweetList />
+//         </Row>
+//       </Container> */}
+//     </div>
+//   )
+// }
+
+// export default App;
